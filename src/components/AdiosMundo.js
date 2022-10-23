c4e6e14a28bdb18b84e072091dab0553ba24d787
Carlos Alfonso Garcia Gonzales <cgarcia@stsac.com>
2022-10-22 21:18:50 -0500

    Creación de componentes

---
+++ src/components/AdiosMundo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdiosMundo = () => {
+    return (
+        <div>
+            <h3>Adios</h3>
+        </div>
+    );
+};
+
+export default AdiosMundo;
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HolaMundo = () => {
+
+    const mifuncion = () => {
+        console.log ('Mi función');
+    }
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <h2>Carlos A. Garcia Gonzales</h2>
+        </>
+    );
+}
+
+export default HolaMundo;
